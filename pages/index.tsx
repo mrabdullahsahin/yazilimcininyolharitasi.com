26f05d865bcfc07fba2b9bf2e4a18f9caca339b9
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-06-30 18:09:37 +0300

    update index page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,41 +2,34 @@ import {
   Box,
   Container,
   Heading,
-  Link,
   SimpleGrid,
   Text,
-  useMediaQuery,
 } from '@chakra-ui/react';
 import { GlobalHeader } from '../components/global-header';
 import { Footer } from '../components/footer';
-import { UpdatesBanner } from '../components/updates-banner';
 import { OpensourceBanner } from '../components/opensource-banner';
 import { DimmedMore } from '../components/dimmed-more';
 import { LinksListItem } from '../components/links-list-item';
-import { VideoIcon } from '../components/icons/video-icon';
 import { LinksList } from '../components/links-list';
 import { HomeRoadmapItem } from '../components/roadmap/home-roadmap-item';
 import { getFeaturedRoadmaps, RoadmapType } from '../lib/roadmap';
 import { getAllGuides, GuideType } from '../lib/guide';
-import { getAllVideos, VideoType } from '../lib/video';
 import siteConfig from '../content/site.json';
 import Helmet from '../components/helmet';
-import { event } from '../lib/gtag';
 import { PageWrapper } from '../components/page-wrapper';
 
 type HomeProps = {
   roadmaps: RoadmapType[];
   guides: GuideType[];
-  videos: VideoType[];
 };
 
 export default function Home(props: HomeProps) {
-  const { roadmaps, guides, videos } = props;
+  const { roadmaps, guides } = props;
 
   return (
     <PageWrapper>
       <GlobalHeader variant={'transparent'} />
-      <Helmet title="Developer Roadmaps" />
+      <Helmet title="Yazılımcının Yol Haritası" />
       <Box>
         <Container maxW="container.md" pb="90px">
           <Box py={['23px', '23px', '35px']} color="gray.200">
@@ -72,50 +65,11 @@ export default function Home(props: HomeProps) {
         </Container>
       </Box>
 
-      <Box bg="white">
-        <Container maxW="container.md">
-          <Box pt="60px" mb={['10px', '15px', '20px']}>
-            <Heading
-              color="green.500"
-              fontSize={['20px', '20px', '25px']}
-              mb="5px"
-            >
-              Video Explanations
-            </Heading>
-          </Box>
-
-          <LinksList>
-            {videos.map((video) => (
-              <LinksListItem
-                target={'_blank'}
-                key={video.id}
-                href={video.youtubeLink!}
-                badgeText={video.isPro ? 'PRO' : ''}
-                hideSubtitleOnMobile
-                title={video.title}
-                subtitle={video.duration}
-                icon={
-                  <VideoIcon
-                    style={{
-                      marginRight: '7px',
-                      width: '18px',
-                      height: '18px',
-                      color: '#9c9c9c',
-                    }}
-                  />
-                }
-              />
-            ))}
-            <DimmedMore href="/watch" text={'View all Videos'} />
-          </LinksList>
-        </Container>
-      </Box>
-
       <Box pb="80px" bg="white">
         <Container maxW="container.md" position="relative">
           <Box pt="40px" mb="20px">
             <Heading color="green.500" fontSize="25px" mb="5px">
-              Visual Guides
+              Kılavuzlar
             </Heading>
           </Box>
 
@@ -129,13 +83,12 @@ export default function Home(props: HomeProps) {
                 subtitle={guide.formattedUpdatedAt!}
               />
             ))}
-            <DimmedMore href={'/guides'} text="View all Guides" />
+            <DimmedMore href={'/guides'} text="Kılavuzları Görüntüle" />
           </LinksList>
         </Container>
       </Box>
 
       <OpensourceBanner />
-      <UpdatesBanner />
       <Footer />
     </PageWrapper>
   );
@@ -146,7 +99,6 @@ export async function getStaticProps() {
     props: {
       roadmaps: getFeaturedRoadmaps(),
       guides: getAllGuides(10),
-      videos: getAllVideos(10),
     },
   };
 }
