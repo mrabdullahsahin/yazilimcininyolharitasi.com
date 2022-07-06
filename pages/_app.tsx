93bc197f7f02d65be9d361d896ae6ccdc21fbcbc
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-07-06 22:52:54 +0300

    delete sticky banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -59,7 +59,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={roadmapTheme}>
       <Global styles={GlobalStyles} />
-      <StickyBanner />
       <Component {...pageProps} />
     </ChakraProvider>
   );
